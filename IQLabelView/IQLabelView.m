20ff42f28c79ab478d0d3ee14f3b5dce654e8a5f
ringtdblai <ringtdblai@gmail.com>
2016-12-07 16:48:30 +0800

    Add testing log

--- IQLabelView/IQLabelView.m
+++ IQLabelView/IQLabelView.m
@@ -470,13 +470,17 @@ static IQLabelView *lastTouchedView;
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
 {
     
+    NSString *text = [textField.text stringByReplacingCharactersInRange:range withString:string];
+    textField.text = text;
+    
+    NSLog(@"text:%@",text);
+    NSLog(@"textField.text:%@",textField.text);
+    
     if (!isShowingEditingHandles) {
         [self showEditingHandles];
     }
     
-    NSString *text = [textField.text stringByReplacingCharactersInRange:range withString:string];
-
-    [textField adjustsWidthWithText:text];
+    [textField adjustsWidthToFillItsContents];
 
     return YES;
 }
